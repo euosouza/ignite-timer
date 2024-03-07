22dd3f1052b9e2eb151fb61e2dbba51191a2b217
Victor Souza <euosouza@gmail.com>
2024-03-06 21:52:41 -0300

    style: add tema e configurando estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/theme/default";
+import { GlobalStyles } from "./styles/global";
+
 export function App() {
   return (
-    <>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --font-type: "Inter", sans-serif;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: var(--font-type);
+  }
+
+  body {
+    line-height: 1;
+    -webkit-font-smoothing: antialiase;
+    background-color: ${({ theme }) => theme["gray-900"]};
+    color: ${({ theme }) => theme.white}
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  a, text-area, button, input {
+    font-size: 1rem;
+    font-family: var(--font-type);
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: 0.2s ease-in;
+  }
+
+  a {
+    text-decoration: none;
+    transition: 0.2s ease-in-out;
+  }
+
+  :focus {
+    outline: transparent;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme["gray-500"]};
+  }
+`
