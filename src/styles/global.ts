0cca5786cd6a79c0501e7df93369405096420b34
Victor Souza <euosouza@gmail.com>
2024-03-12 17:57:38 -0300

    style: estilizando scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -30,16 +30,53 @@ export const GlobalStyles = createGlobalStyle`
 
   button, a {
     cursor: pointer;
-    transition: 0.2s ease-in;
+    box-sizing: border-box;
+    transition: 0.3s ease-in;
   }
 
   a {
     text-decoration: none;
-    transition: 0.2s ease-in-out;
+    transition: 0.3s ease-in-out;
   }
 
   :focus {
     outline: transparent;
-    box-shadow: 0 0 0 2px ${({ theme }) => theme["gray-500"]};
+    /* box-shadow: 0 0 0 2px ${({ theme }) => theme["gray-500"]}; */
+  }
+
+  *{
+    scrollbar-width: thin;
+    scrollbar-color: #3A3A41 #5A5A65;
+  }
+
+  /* Chrome, Edge and Safari */
+  *::-webkit-scrollbar {
+    height: 10px;
+    width: 10px;
+  }
+  *::-webkit-scrollbar-track {
+    border-radius: 5px;
+    background-color: #5A5A65;
+  }
+
+  *::-webkit-scrollbar-track:hover {
+    background-color: #5A5A65;
+  }
+
+  *::-webkit-scrollbar-track:active {
+    background-color: #5A5A65;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: #3A3A41;
+  }
+
+  *::-webkit-scrollbar-thumb:hover {
+    background-color: #202024;
+  }
+
+  *::-webkit-scrollbar-thumb:active {
+    background-color: #202024;
   }
 `;
