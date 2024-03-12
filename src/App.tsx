854777ba44954b22cc68e023de622c7a8c761e27
Victor Souza <euosouza@gmail.com>
2024-03-12 17:55:27 -0300

    feat: criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyles } from "./styles/global";
+
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
