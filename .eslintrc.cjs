744dc7e00e2f79d1122b0a2578fcf12cba6ddb91
Victor Souza <euosouza@gmail.com>
2024-03-15 13:45:29 -0300

    stryle: corrigindo regras do eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'max-len': ["error", { "code": 80 }],
+    'max-len': ["error", { "code": 120 }],
     'quotes': ["error", "double"],
     'semi': ["error", "always"]
   },
