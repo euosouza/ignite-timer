2eee1ada330d8400c9cf4989815aba0c304816be
Victor Souza <euosouza@gmail.com>
2024-03-15 13:49:11 -0300

    refactor: ajustando tela history para utilizar o cycle-context e o cycle-reducer

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import { HistoryContainer, ContainerTable, Status } from "./styles";
+import { CyclesContext } from "../../context/CyclesContext";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 export function History() {
+  const { cycles } = useContext(CyclesContext);
+
   return(
     <HistoryContainer>
       <h1>Meu histórico</h1>
@@ -16,26 +22,22 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Estudando React</td>
-              <td>25 minutos</td>
-              <td>Há certa de 2 meses</td>
-              <td><Status $statuscolor="green">Concluído</Status></td>
-            </tr>
-
-            <tr>
-              <td>Estudando React</td>
-              <td>25 minutos</td>
-              <td>Há certa de 2 meses</td>
-              <td><Status $statuscolor="yellow">Em andamento</Status></td>
-            </tr>
-
-            <tr>
-              <td>Estudando React</td>
-              <td>25 minutos</td>
-              <td>Há certa de 2 meses</td>
-              <td><Status $statuscolor="red">Interrompido</Status></td>
-            </tr>
+            { cycles.map((cycle) =>
+              (<tr key={cycle.id}>
+                <td>{ cycle.task }</td>
+                <td>{ cycle.minutesAmount } minutos</td>
+                <td>{ formatDistanceToNow(cycle.startDate, { addSuffix: true, locale: ptBR }) }</td>
+                <td>
+                  { cycle.finishedDate && <Status $statuscolor="green">Concluído</Status> }
+                  { cycle.interruptedDate && <Status $statuscolor="red">Interrompido</Status> }
+                  { !cycle.finishedDate && !cycle.interruptedDate &&
+                  <Status $statuscolor="yellow">
+                    Em Andamento
+                  </Status> }
+                </td>
+              </tr>)
+              )
+            }
           </tbody>
         </table>
       </ContainerTable>
